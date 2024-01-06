6d04c8dc97e263c2065fadc925830c5ed320e1a3
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-01-06 16:35:09 +0000

    Add tasks and pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
+// Requirements
+// ------------
+
+// Display the current day at the top of the calender when a user opens the planner.
+
+// Present time blocks for standard business hours when the user scrolls down.
+
+// Color-code each time block based on past, present, and future when the time block is viewed.
+
+// Allow a user to enter an event when they click a time block
+
+// Save the event in local storage when the save button is clicked in that time block.
+
+// Persist events between refreshes of a page
+
+
+// Pseudocode
+// ----------
+
+// function to add today's date in the currentDay tag (once on load)
+
+// function to get data from localStorage and save in a variable for populating time-blocks
+
+// function to render time blocks during work hours, add descriptions and appropriate past/present/future classes for each row
+
+// Add event listener to save button clicks for saving hour and description data to localStorage
+
+// Constants for both start and end hours
 
