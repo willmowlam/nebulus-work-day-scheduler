760d9eeba35a752c3576688d6a63b2ce4096810d
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-01-07 17:13:46 +0000

    Remove requirements comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,3 @@
-// Requirements
-// ------------
-
-// !Display the current day at the top of the calender when a user opens the planner.
-
-// !Present time blocks for standard business hours when the user scrolls down.
-
-// !Color-code each time block based on past, present, and future when the time block is viewed.
-
-// !Allow a user to enter an event when they click a time block
-
-// !Save the event in local storage when the save button is clicked in that time block.
-
-// !Persist events between refreshes of a page
-
-
 // Set start and end work hours as integers (9 = 9am, 17 = 5pm)
 const intStartTime = 0;
 const intEndTime = 23;
